10d522ecf1d4c629492a81877a4ee8812e1b72c6
404950139 <404950139@qq.com>
2019-08-06 09:21:11 +0800

    dbUtil.js

--- dao/dbUtil.js
+++ dao/dbUtil.js
@@ -2,10 +2,15 @@ const mysql = require("mysql");
 
 let createConnection = () => {
     const connection = mysql.createConnection({
-        host: "localhost",
+        // host: "localhost",
+        // port: "3306",
+        // user: "root",
+        // password: "shi1505223675",
+        // database: "student"
+        host: "39.108.104.92",
         port: "3306",
-        user: "root",
-        password: "shi1505223675",
+        user: "student",
+        password: "BCJPMdSwfxYPkWZt",
         database: "student"
     });
     return connection;
